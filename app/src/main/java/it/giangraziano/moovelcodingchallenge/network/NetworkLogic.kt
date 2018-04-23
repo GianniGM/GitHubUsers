abcd2403b0023983d0e4016e6ea71de6fb9ad790
GianniGM <giangraziano@gmail.com>
2018-04-23 23:29:31 +0200

    added rxJava

---
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/network/NetworkLogic.kt
@@ -0,0 +1,23 @@
+package it.giangraziano.moovelcodingchallenge.network
+
+import io.reactivex.Single
+import it.giangraziano.moovelcodingchallenge.model.GitHubUser
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkLogic {
+    private val retrofit = Retrofit.Builder()
+            .baseUrl(NetworkData.baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(GitHubService::class.java)
+
+
+    fun getGitHubUsersFromApi(): Single<MutableList<GitHubUser>>? {
+        return retrofit.getDevelopers(
+                135
+        )
+    }
+}
