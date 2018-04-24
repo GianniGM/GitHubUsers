50be41f6fa2c26b5443217e6e976325a31d4af93
GianniGM <giangraziano@gmail.com>
2018-04-24 22:00:40 +0200

    changed api from /users to /search/users

--- app/src/main/java/it/giangraziano/moovelcodingchallenge/network/GitHubNetwork.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/network/GitHubNetwork.kt
@@ -2,6 +2,7 @@ package it.giangraziano.moovelcodingchallenge.network
 
 import io.reactivex.Single
 import it.giangraziano.moovelcodingchallenge.model.GitHubUser
+import it.giangraziano.moovelcodingchallenge.model.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -17,7 +18,10 @@ class GitHubNetwork(private val retrofit: GitHubService) {
                 .create(GitHubService::class.java) )
     }
 
-    fun getGitHubUsersFromApi(): Single<MutableList<GitHubUser>>? {
-        return retrofit.getDevelopers(135)
-    }
+    fun getGitHubUsersFromApi(): Single<Response>? =
+            retrofit.searchDevelopers( NetworkData.query, 0,NetworkData.type)
+
+
+    fun getGitHubUserInfo(username: String): Single<GitHubUser>? =
+            retrofit.getDeveloper(username)
 }
--- app/src/main/java/it/giangraziano/moovelcodingchallenge/network/GitHubService.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/network/GitHubService.kt
@@ -2,10 +2,18 @@ package it.giangraziano.moovelcodingchallenge.network
 
 import io.reactivex.Single
 import it.giangraziano.moovelcodingchallenge.model.GitHubUser
+import it.giangraziano.moovelcodingchallenge.model.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GitHubService {
     @GET("/users")
-    fun getDevelopers(@Query("since") since: Int): Single<MutableList<GitHubUser>>
+    fun getDeveloper(@Query("username") username: String): Single<GitHubUser>
+
+    @GET("/search/users")
+    fun searchDevelopers(
+            @Query("q") q: String,
+            @Query("page") page: Int,
+            @Query("type") type: String
+    ): Single<Response>
 }
