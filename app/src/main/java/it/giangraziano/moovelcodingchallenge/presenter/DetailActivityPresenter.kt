90c3a918952369d58b932d5fc693d3e942b61b4e
GianniGM <giangraziano@gmail.com>
2018-04-24 23:11:02 +0200

    changed the way I pass username on Presenter

--- app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/DetailActivityPresenter.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/DetailActivityPresenter.kt
@@ -1,8 +1,6 @@
 package it.giangraziano.moovelcodingchallenge.presenter
 
-import it.giangraziano.moovelcodingchallenge.model.GitHubUser
-
 interface DetailActivityPresenter {
-    fun onResume(username: String)
+    fun onResume()
     fun onDestroy()
 }
--- app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/DetailActivityPresenterImpl.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/DetailActivityPresenterImpl.kt
@@ -6,21 +6,24 @@ import io.reactivex.schedulers.Schedulers
 import it.giangraziano.moovelcodingchallenge.network.GitHubNetwork
 import it.giangraziano.moovelcodingchallenge.view.DetailView
 
-class DetailActivityPresenterImpl(private val detailView: DetailView) : DetailActivityPresenter {
+class DetailActivityPresenterImpl(
+        private val detailView: DetailView,
+        private val userLogin: String
+) : DetailActivityPresenter {
 
     private val network = GitHubNetwork.create()
     private var obs: Disposable? = null
 
-    override fun onResume(username: String) {
-        serve(username)
+    override fun onResume() {
+        serve()
     }
 
     override fun onDestroy() {
         obs?.dispose()
     }
 
-    private fun serve(username: String) {
-        this.obs = network.getGitHubUserInfo(username)
+    private fun serve() {
+        this.obs = network.getGitHubUserInfo(userLogin)
                 ?.subscribeOn(Schedulers.io())
                 ?.observeOn(AndroidSchedulers.mainThread())
                 ?.subscribe({
