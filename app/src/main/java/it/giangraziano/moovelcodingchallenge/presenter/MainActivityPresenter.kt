c4ea3f7ab4c91287cca6a1a9a124e676348a51f6
Gianni G <giangraziano@gmail.com>
2018-04-24 16:38:47 +0200

    added presenter

---
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/MainActivityPresenter.kt
@@ -0,0 +1,9 @@
+package it.giangraziano.moovelcodingchallenge.presenter
+
+interface MainActivityPresenter {
+    fun onResume()
+
+    fun onRecyclerViewScrollToEnd()
+
+    fun onDestroy()
+}
---
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/MainActivityPresenterImpl.kt
@@ -0,0 +1,41 @@
+package it.giangraziano.moovelcodingchallenge.presenter
+
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import it.giangraziano.moovelcodingchallenge.view.MainView
+import it.giangraziano.moovelcodingchallenge.network.GitHubNetwork
+
+class MainActivityPresenterImpl(private val mainView: MainView) : MainActivityPresenter{
+
+    private val network = GitHubNetwork.create()
+    private var obs: Disposable? = null
+
+    override fun onRecyclerViewScrollToEnd() {
+        serve()
+    }
+
+    override fun onResume() {
+        mainView.showProgress()
+        serve()
+    }
+
+    private fun serve() {
+        this.obs = network.getGitHubUsersFromApi()
+                ?.subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribe({
+                    mainView.hideProgress(true)
+                    val developers = it.asSequence().sortedBy { it.login }.toMutableList()
+                    mainView.setData(developers)
+                }, {
+                    mainView.hideProgress(false)
+                    mainView.showToast(it.localizedMessage.toString())
+                })
+    }
+
+    override fun onDestroy() {
+        this.obs?.dispose()
+    }
+
+}
