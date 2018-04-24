2d312d3d6b86e87f5cd7142ede9afb6ec0fff2ce
GianniGM <giangraziano@gmail.com>
2018-04-24 22:10:28 +0200

    added Detail Activity view interface

---
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/view/DetailView.kt
@@ -0,0 +1,8 @@
+package it.giangraziano.moovelcodingchallenge.view
+
+import it.giangraziano.moovelcodingchallenge.model.GitHubUser
+
+interface DetailView {
+    fun showToast(message: String)
+    fun setView(user: GitHubUser)
+}
