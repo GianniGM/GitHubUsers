548b3577409b15810bb728848c495451984aa1de
GianniGM <giangraziano@gmail.com>
2018-04-24 22:47:19 +0200

    changed malformed retrofit request

--- app/src/main/java/it/giangraziano/moovelcodingchallenge/network/GitHubService.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/network/GitHubService.kt
@@ -4,11 +4,12 @@ import io.reactivex.Single
 import it.giangraziano.moovelcodingchallenge.model.GitHubUser
 import it.giangraziano.moovelcodingchallenge.model.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GitHubService {
-    @GET("/users")
-    fun getDeveloper(@Query("username") username: String): Single<GitHubUser>
+    @GET("/users/{username}")
+    fun getDeveloper(@Path("username") username: String): Single<GitHubUser>
 
     @GET("/search/users")
     fun searchDevelopers(
