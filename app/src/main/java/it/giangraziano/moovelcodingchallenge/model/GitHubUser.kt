06dfc84c0fb83145ed7407c312df2df43649e4bc
GianniGM <giangraziano@gmail.com>
2018-04-24 21:59:39 +0200

    added additional info about user and apis, added response class provided by searchDeveloper api

--- app/src/main/java/it/giangraziano/moovelcodingchallenge/model/GitHubUser.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/model/GitHubUser.kt
@@ -1,10 +1,19 @@
 package it.giangraziano.moovelcodingchallenge.model
 
+data class Response(
+        val total_count: Long?,
+        val incomplete_results: Boolean?,
+        val items: MutableList<GitHubUser>?
+)
+
 data class GitHubUser(
         val login: String?,
         val id: Int?,
         val avatar_url: String?,
         val url: String?,
         val html_url: String,
-        val type: String
+        val type: String,
+        val followers: Long?,
+        val created_at: String?,
+        val updated_at: String
 )
