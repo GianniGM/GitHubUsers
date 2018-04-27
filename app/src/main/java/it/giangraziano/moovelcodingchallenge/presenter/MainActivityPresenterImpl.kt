be298e72bce1934fbabe9549c4242760e0de7f15
Gianni G <giangraziano@gmail.com>
2018-04-27 16:02:49 +0200

    added business logic that implements paging functionality: * add 10 users at once when i scroll * if i raised maximum used downloaded then download next page

--- app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/MainActivityPresenterImpl.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/MainActivityPresenterImpl.kt
@@ -3,33 +3,71 @@ package it.giangraziano.moovelcodingchallenge.presenter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import it.giangraziano.moovelcodingchallenge.model.GitHubUser
 import it.giangraziano.moovelcodingchallenge.view.MainView
 import it.giangraziano.moovelcodingchallenge.network.GitHubNetwork
+import kotlin.properties.Delegates
 
-class MainActivityPresenterImpl(private val mainView: MainView) : MainActivityPresenter{
+const val USERS_TO_LOAD_AT_ONCE = 10
+
+class MainActivityPresenterImpl(private val mainView: MainView) : MainActivityPresenter {
 
     private val network = GitHubNetwork.create()
     private var obs: Disposable? = null
+    private var currentPage = 1
 
-    override fun onRecyclerViewScrollToEnd() {
-        serve()
+    private var currentCount = 0
+    private var totalCountPerPage = 0
+    private var currentDevelopers by Delegates.observable(mutableListOf<GitHubUser>()) { _, _, dev ->
+        currentCount = 0
+        totalCountPerPage = dev.size
+        loadUsers()
     }
 
     override fun onResume() {
         mainView.showProgress()
-        serve()
+        loadUsers()
+    }
+
+    override fun onScrollToEnd() {
+        loadUsers()
+    }
+
+    override fun onRecyclerViewScrollToEnd() {
+        loadUsers()
+    }
+
+    private fun loadUsers() {
+
+        val offset = calculateOffsetIfLessThenDefault()
+
+        if (currentCount < totalCountPerPage) {
+            val newList = currentDevelopers.subList(currentCount, currentCount + offset)
+            mainView.addData(newList)
+            currentCount += USERS_TO_LOAD_AT_ONCE
+        } else {
+            serve()
+        }
+    }
+
+    private fun calculateOffsetIfLessThenDefault(): Int {
+        return if (totalCountPerPage - currentCount < USERS_TO_LOAD_AT_ONCE) {
+            totalCountPerPage - currentCount
+        } else {
+            USERS_TO_LOAD_AT_ONCE
+        }
     }
 
     private fun serve() {
-        this.obs = network.getGitHubUsersFromApi()
+        this.obs = network.getGitHubUsersFromApi(currentPage++)
                 ?.subscribeOn(Schedulers.io())
                 ?.observeOn(AndroidSchedulers.mainThread())
                 ?.subscribe({
-                    mainView.hideProgress(true)
                     val developers = it.items?.asSequence()?.sortedBy { it.login }?.toMutableList()
                     if (developers != null) {
-                        mainView.setData(developers)
+                        currentDevelopers = developers
                     }
+                    mainView.hideProgress(true)
                 }, {
                     mainView.hideProgress(false)
                     mainView.showToast(it.localizedMessage.toString())
