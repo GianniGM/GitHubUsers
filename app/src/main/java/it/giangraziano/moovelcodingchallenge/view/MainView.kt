e7ef595cf4943a9816e64c547cdbd31242904992
Gianni G <giangraziano@gmail.com>
2018-04-27 16:03:37 +0200

    changed Refactored method's name from setData to addData

--- app/src/main/java/it/giangraziano/moovelcodingchallenge/view/MainView.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/view/MainView.kt
@@ -6,6 +6,6 @@ import it.giangraziano.moovelcodingchallenge.model.GitHubUser
 interface MainView {
     fun showProgress()
     fun hideProgress(loadingSuccess: Boolean)
-    fun setData(dataList: MutableList<GitHubUser>)
+    fun addData(dataList: MutableList<GitHubUser>)
     fun showToast(message:String)
 }
