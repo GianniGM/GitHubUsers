2378154efd6dca6a58857516fd3dff26e8c5544e
GianniGM <giangraziano@gmail.com>
2018-04-29 14:52:15 +0200

    code refactored: renamed values

--- app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/DetailActivityPresenterImpl.kt
+++ app/src/main/java/it/giangraziano/moovelcodingchallenge/presenter/DetailActivityPresenterImpl.kt
@@ -3,15 +3,15 @@ package it.giangraziano.moovelcodingchallenge.presenter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import it.giangraziano.moovelcodingchallenge.model.GitHubState
-import it.giangraziano.moovelcodingchallenge.view.DetailView
+import it.giangraziano.moovelcodingchallenge.model.GitHubStateImpl
+import it.giangraziano.moovelcodingchallenge.detailView.DetailView
 
 class DetailActivityPresenterImpl(
         private val detailView: DetailView,
         private val userLogin: String
 ) : DetailActivityPresenter {
 
-    private val network = GitHubState.create()
+    private val state = GitHubStateImpl.create()
     private var disposable: Disposable? = null
 
     override fun onResume() {
@@ -23,7 +23,7 @@ class DetailActivityPresenterImpl(
     }
 
     private fun serve() {
-        this.disposable = network.getGitHubUserInfo(userLogin)
+        this.disposable = state.getGitHubUserInfo(userLogin)
                 ?.subscribeOn(Schedulers.io())
                 ?.observeOn(AndroidSchedulers.mainThread())
                 ?.subscribe({
